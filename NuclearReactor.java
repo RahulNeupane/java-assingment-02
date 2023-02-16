424585ad5e71f00d4e1588a36de36fde4d91dfb8
RahulNeupane <rahulneupane0090@gmail.com>
2023-02-16 10:08:25 +0545

    updated

--- NuclearReactor.java
+++ NuclearReactor.java
@@ -20,11 +20,12 @@ public class NuclearReactor {
         System.out.println("");
         System.out.println("Please Fill Following Form");
         System.out.println("");
+        Thread.sleep(500); // Pause for half a second
 
         System.out.println("Q1. What is the total number of employees? ");
         System.out.print("-> ");
         int totalEmp = input.nextInt();
-
+        
         System.out.println("Q2. What is the name of supervisor ?");
         input.nextLine();
         System.out.print("-> ");
@@ -33,21 +34,29 @@ public class NuclearReactor {
         System.out.println("Q3. How many times the Nuclear waste is removed from the core?");
         System.out.print("-> ");
         int times = input.nextInt();
-
+        
         System.out.println("Q4. What is the total weight of the waste produced every 1 week?");
         System.out.print("-> ");
         float weight = input.nextFloat();
-
+        
         System.out.println("Q5. Is Electric Motor being replaced every 18 days? ");
         System.out.println("Enter “y” for yes and “n” for no ");
         System.out.print("-> ");
         char replaced = input.next().charAt(0);
-
+        System.out.println("");
+        System.out.println("Invalid input provided");
+        if(replaced != 'y' || replaced != 'n'){
+            System.out.println("Q5. Is Electric Motor being replaced every 18 days? ");
+            System.out.println("Enter “y” for yes and “n” for no ");
+            System.out.print("-> ");
+            char replaced2 = input.next().charAt(0);
+            replaced = replaced2;
+        }
         System.out.println("Q6. What is the core average temperature ( celsius ) of the Nuclear Reactor? ");
         System.out.print("-> ");
         double temp = input.nextDouble();
-
-
+        
+        
         System.out.println("");
         System.out.println("");
         
@@ -63,7 +72,7 @@ public class NuclearReactor {
             System.out.println("Electric Motor is not being replaced every 18 days.");
         }
         System.out.print("The core average temperature of the Nuclear Reactor: " + temp+ "°C" + ", ");
-        System.out.print((temp+273.15) + "°F");
+        System.out.print((temp+273.15) + "°K");
         System.out.println("");
         System.out.println("");
         input.close();
